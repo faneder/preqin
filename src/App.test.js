b6fb4def1d39a16ff06bd788635f7ee010667fef
Eder Fan <fanedercode@gmail.com>
2023-12-06 22:13:16 +0000

    Add react route for navigating to different components

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,37 @@
-import { render, screen } from '@testing-library/react';
+import {render, screen, waitForElementToBeRemoved} from '@testing-library/react';
 import App from './App';
+import React from "react";
+import {getInvestors} from "./api/investorApi";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+jest.mock('./api/investorApi');
+
+const mockNavigate = jest.fn();
+
+jest.mock('react-router-dom', () => ({
+    ...jest.requireActual('react-router-dom'),
+    useNavigate: () => mockNavigate,
+}));
+
+const investorsResponse = {
+    "data": [
+        {
+            firmID: '1111',
+            firmName: 'Firm 1',
+            firmType: 'Firm Type 1',
+            address: 'Address 1',
+        },
+    ]
+};
+
+test('renders investor component as default page', async () => {
+    getInvestors.mockResolvedValue(investorsResponse);
+
+    render(<App/>);
+
+    const loader = await screen.findByTestId('loader');
+    expect(loader).toBeInTheDocument();
+    await waitForElementToBeRemoved(() => screen.queryByTestId('loader'));
+
+    const errorMessage = await screen.findByText('Investors');
+    expect(errorMessage).toBeInTheDocument();
 });
