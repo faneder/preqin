f0767deab4ab237ad0fd65216bfc750ec6e1a557
Eder Fan <fanedercode@gmail.com>
2023-12-06 20:45:05 +0000

    Create investor page to display commitments

---
+++ src/components/InvestorPage/InvestorPage.js
@@ -0,0 +1,102 @@
+import React, {useEffect, useState} from 'react';
+import {useParams} from 'react-router-dom';
+import {getInvestorCommitments} from '../../api/investorApi';
+
+const InvestorPage = () => {
+    const {investorId} = useParams();
+    const [assetClasses] = useState([
+        {label: 'Private Equity', value: 'pe'},
+        {label: 'Private Debt', value: 'pd'},
+        {label: 'Real Estate', value: 're'},
+        {label: 'Infrastructure', value: 'inf'},
+        {label: 'Natural Resources', value: 'nr'},
+        {label: 'Hedge Funds', value: 'hf'}
+    ]);
+    const [selectedAssetClass, setSelectedAssetClass] = useState(assetClasses[0]);
+    const [commitments, setCommitments] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            if (selectedAssetClass && investorId) {
+                try {
+                    const response = await getInvestorCommitments(selectedAssetClass.value, investorId);
+                    setCommitments(response.data);
+                } catch (error) {
+                    setError(error);
+                } finally {
+                    setIsLoading(false);
+                }
+            }
+        };
+
+        fetchData();
+    }, [selectedAssetClass, investorId]);
+
+    const handleAssetClassChange = (event) => {
+        setSelectedAssetClass(
+            assetClasses.find(a => a.value === event.target.value)
+        );
+    }
+
+    if (isLoading) {
+        return <p>Loading...</p>;
+    }
+
+    if (error) return <p>{error.message}</p>;
+
+    return (
+        <div>
+            <h3 className="caption">Commitments for {selectedAssetClass.label}</h3>
+            <b>Select an asset class:</b>
+            <select onChange={handleAssetClassChange} value={selectedAssetClass}>
+                {assetClasses.map((assetClass) => (
+                    <option key={assetClass.value} value={assetClass.value}>
+                        {assetClass.label}
+                    </option>
+                ))}
+            </select>
+
+            <table className="investors-table">
+                <thead>
+                <tr>
+                    <th>Investor Name</th>
+                    <th>Fun ID</th>
+                    <th>Fund Name</th>
+                    <th>Fund Manager Name</th>
+                    <th>FUND SIZE MN</th>
+                    <th>Currency</th>
+                    <th>Vintage</th>
+                    <th>Fund Type</th>
+                    <th>Core Industries</th>
+                    <th>Industry Verticals</th>
+                    <th>Benchmark Locations</th>
+                    <th>Manager Experience</th>
+                </tr>
+                </thead>
+                <tbody>
+                {commitments.length > 0 ? (commitments.map((commitment, index) => (
+                    <tr role="row" key={index} data-testid="investor-row">
+                        <td data-testid="investor-name">{commitment.investorName}</td>
+                        <td data-testid="fund-id">{commitment.fundId}</td>
+                        <td data-testid="fund-name">{commitment.fundName}</td>
+                        <td data-testid="fund-manager-name">{commitment.fundManagerName}</td>
+                        <td data-testid="fund-size">{commitment.fundSizeMn}</td>
+                        <td data-testid="fund-currency">{commitment.fundCurrency}</td>
+                        <td data-testid="vintage">{commitment.vintage}</td>
+                        <td data-testid="fund-type">{commitment.fundType}</td>
+                        <td data-testid="core-industries">{commitment.coreIndustries}</td>
+                        <td data-testid="industry-verticals">{commitment.industryVerticals}</td>
+                        <td data-testid="benchmark-locations">{commitment.benchmarkLocations}</td>
+                        <td data-testid="manager-experience">{commitment.managerExperience}</td>
+                    </tr>
+                    ))
+                ) : 'No commitments found'}
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
+export default InvestorPage;
---
+++ src/components/InvestorPage/InvestorPage.test.js
@@ -0,0 +1,130 @@
+import React from 'react';
+import {fireEvent, render, screen, within} from '@testing-library/react';
+import {useParams} from 'react-router-dom';
+import {getInvestorCommitments} from '../../api/investorApi';
+import InvestorDetails from './InvestorPage';
+
+jest.mock('react-router-dom', () => ({
+    ...jest.requireActual('react-router-dom'),
+    useParams: jest.fn(),
+}));
+
+jest.mock('../../api/investorApi');
+
+const commitmentsResponse = {
+    "data": [{
+        "investorId": "2670",
+        "investorName": "1199SEIU National Benefit Fund",
+        "fundId": "2124",
+        "fundName": "American Securities Partners II",
+        "fundManagerId": "44",
+        "fundManagerName": "American Securities",
+        "fundCurrency": "USD",
+        "fundSizeMn": "350.00",
+        "committedMn": "",
+        "vintage": "1998",
+        "fundType": "Buyout",
+        "coreIndustries": "Consumer Discretionary",
+        "industryVerticals": "Manufacturing",
+        "domicile": "",
+        "benchmarkLocations": "North America",
+        "managerExperience": "2nd Fund"
+    }]
+}
+
+describe('InvestorPage', () => {
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    it('displays investor commitment are fetched in table when calls API', async () => {
+        mockUseParams(777);
+        getInvestorCommitments.mockResolvedValue(commitmentsResponse);
+
+        render(<InvestorDetails/>);
+
+        const table = await screen.findByRole('table');
+        expect(table).toBeInTheDocument();
+
+        const rows = screen.getAllByTestId('investor-row');
+
+        rows.forEach((row) => {
+            const name = within(row).getByTestId('investor-name');
+            const id = within(row).getByTestId('fund-id');
+            const fundName = within(row).getByTestId('fund-name');
+            const fundManagerName = within(row).getByTestId('fund-manager-name');
+            const fundSize = within(row).getByTestId('fund-size');
+            const fundCurrency = within(row).getByTestId('fund-currency');
+            const vintage = within(row).getByTestId('vintage');
+            const fundType = within(row).getByTestId('fund-type');
+            const coreIndustries = within(row).getByTestId('core-industries');
+            const industryVerticals = within(row).getByTestId('industry-verticals');
+            const benchmarkLocations = within(row).getByTestId('benchmark-locations');
+            const managerExperience = within(row).getByTestId('manager-experience');
+
+            expect(name).toHaveTextContent('1199SEIU National Benefit Fund');
+            expect(id).toHaveTextContent('2124');
+            expect(fundName).toHaveTextContent('American Securities Partners II');
+            expect(fundManagerName).toHaveTextContent('American Securities');
+            expect(fundSize).toHaveTextContent('350.00');
+            expect(fundCurrency).toHaveTextContent('USD');
+            expect(vintage).toHaveTextContent('1998');
+            expect(fundType).toHaveTextContent('Buyout');
+            expect(coreIndustries).toHaveTextContent('Consumer Discretionary');
+            expect(industryVerticals).toHaveTextContent('Manufacturing');
+            expect(benchmarkLocations).toHaveTextContent('North America');
+            expect(managerExperience).toHaveTextContent('2nd Fund');
+        });
+    });
+
+    it('displays errors when the API call fails', async () => {
+        mockUseParams(777);
+        getInvestorCommitments.mockRejectedValue(new Error('An error occurred'));
+
+        render(<InvestorDetails/>);
+
+        const errorMessage = await screen.findByText('An error occurred');
+        expect(errorMessage).toBeInTheDocument();
+    });
+
+    it('updates data when selected asset class changes', async () => {
+        mockUseParams(777);
+        getInvestorCommitments
+            .mockResolvedValueOnce({data: [{investorId: '1111'}]})
+            .mockResolvedValueOnce({data: [{investorId: '2222'}]});
+
+        render(<InvestorDetails/>);
+
+        const table1 = await screen.findByRole('table');
+        expect(table1).toBeInTheDocument();
+
+        fireEvent.change(screen.getByDisplayValue('Private Equity'), {target: {value: 'pd'}});
+
+        const table2 = await screen.findByRole('table');
+        expect(table2).toBeInTheDocument();
+    });
+
+    it('displays message when no commitments found', async () => {
+        mockUseParams(777);
+        getInvestorCommitments.mockResolvedValue({"data": []});
+
+        render(<InvestorDetails/>);
+
+        const noCommitments = await screen.findByText('No commitments found');
+        expect(noCommitments).toBeInTheDocument();
+    });
+
+    it('does not fetch commitments when no investorId is provided', async () => {
+        mockUseParams(null);
+
+        render(<InvestorDetails/>);
+
+        await new Promise(r => setTimeout(r, 100));
+
+        expect(getInvestorCommitments).not.toHaveBeenCalled();
+    });
+});
+
+const mockUseParams = (investorId = null) => {
+    useParams.mockReturnValue({investorId});
+}
